bf5e6cd039ae62785fdcd752d4775d77fe616270
LukasMod <lukasz.adam.modzelewski@gmail.com>
2022-10-28 08:33:21 +0200

    add create screen with input

--- src/screens/CreateScreen.tsx
+++ src/screens/CreateScreen.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+import Button from '../components/Button';
 import {colors} from '../theme/colors';
+import {ITodo} from '../types/Todo';
 
 const CreateScreen = () => {
+  const [text, setText] = useState('Test todo');
+
+  const onPressAdd = async () => {
+    const newTodo: ITodo = {
+      id: new Date().getTime(),
+      title: text,
+      date: new Date().toISOString(),
+      done: false,
+    };
+
+    // api call newTodo
+    console.log('TEST newTodo', newTodo);
+
+    setText('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>CREATE SCREEN</Text>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        style={styles.input}
+        placeholder="Type your message..."
+      />
+      <View style={styles.buttonsContainer}>
+        <Button title="ADD TODO" onPress={onPressAdd} />
+      </View>
     </View>
   );
 };
@@ -14,9 +40,21 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 32,
     paddingHorizontal: 24,
+    flex: 1,
   },
-  text: {
-    color: colors.red,
+  buttonsContainer: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  input: {
+    backgroundColor: colors.white,
+    padding: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
+    borderRadius: 50,
+    borderColor: colors.gray3,
+    borderWidth: StyleSheet.hairlineWidth,
   },
 });
 
